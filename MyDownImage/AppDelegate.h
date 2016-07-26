5a8418fe216ca7e8d78270efad684431d34a384a
ldd <964991296@qq.com>
2016-07-26 09:13:12 +0800

    Initial Commit

---
+++ MyDownImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDownImage
+//
+//  Created by ldd on 16/7/26.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDownImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDownImage
+//
+//  Created by ldd on 16/7/26.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
